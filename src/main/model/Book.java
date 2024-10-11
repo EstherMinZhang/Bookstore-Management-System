dd56ee483a439bada3a2dd7ba6c38cfb31e0d9b8
Esther Zhang <mazhang65@students.csubc.ca>
2024-10-10 22:32:54 -0700

    Added specification of Book class

--- src/main/model/Book.java
+++ src/main/model/Book.java
@@ -1,43 +1,82 @@
 package model;
-
+/**
+ * Represents a book with a name, author and price.
+ * Provides getter and setter methods to access and modify its fields.
+ */
 public class Book {
+    private String bookName;
+    private String author;
+    private double price;
 
+    /**
+     * EFFECTS: Initializes a new Book object without the given bookName, author, and price.
+    */
+    public Book(){}
 
-
+    /**
+     * REQUIRES: bookName, author cannot be null; price >= 0
+     * MODIFIES: this
+     * EFFECTS: initializes a new Book object with the given bookName, author and pricae.
+     */
     public Book(String bookName, String author, double price) {
-
+        this.bookName = bookName;
+        this.author = author;
+        this.price = price;
     }
 
-
+    /**
+     * EFFECTS: return the current book name.
+     */
     public String getBookName() {
-       //stub
+       return bookName;
     }
-
-
+    /**
+     * REQUIRES: bookName cannot be null
+     * MODIFIES: this
+     * EFFECTS: Sets the book name to the given value.
+     */
     public void setBookName(String bookName) {
-        //stub
+        this.bookName = bookName;
     }
 
-
+    /**
+     * EFFECTS: return the current author name.
+     */
     public String getAuthor() {
-       //stub
+       return author;
     }
 
-
+    /**
+     * REQUIRES: author cannot be null
+     * MODIFIES: this
+     * EFFECTS: Sets the author to the given value.
+     */
     public void setAuthor(String author) {
-       //stub
+       this.author = author;
     }
 
-
+    /**
+     * EFFECTS: return the current book price.
+     */
     public double getPrice() {
-        //stub
+        return price;
     }
 
+    /**
+     * REQUIRES: prive >= 0.
+     * MODIFIES: this
+     * EFFECTS: Sets the price to the given value.
+     */
     public void setPrice(double price) {
-        //stub
+        this.price = price;
     }
 
+    /**
+     * EFFECTS: return the information of the books,including bookName, author and price.
+     */
     public String toString() {
-        //stub
+        return "The book name is " + bookName + ".\n" +
+               "The author of the book is " + author + ".\n" +
+               "The price of the book is " + price + ".\n";
     }
 }
