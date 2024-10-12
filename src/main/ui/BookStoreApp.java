84be0543e817a5c458fdc089037165c073bd2c43
Esther Zhang <mazhang65@students.csubc.ca>
2024-10-11 20:58:30 -0700

    Fixed Checkstyle issue

--- src/main/ui/BookStoreApp.java
+++ src/main/ui/BookStoreApp.java
@@ -30,8 +30,8 @@ public class BookStoreApp {
      * MODIFIES: this.books
      * EFFECTS: Executes add, remove, edit, or show books from user slection.
      */
-    public void init() {
-        while(true) {
+   public void init() {
+        while (true) {
             System.out.println("1. Add a book.");
             System.out.println("2. Remove a book.");
             System.out.println("3. Edit a book.");
